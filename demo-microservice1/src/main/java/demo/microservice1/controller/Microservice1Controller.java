b44a30170aeaba3dcc9c7e524424c7cc4151d348
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2023-03-26 00:07:23 +0530

    Added changes

--- demo-microservice1/src/main/java/demo/microservice1/controller/Microservice1Controller.java
+++ demo-microservice1/src/main/java/demo/microservice1/controller/Microservice1Controller.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -26,12 +25,12 @@ public class Microservice1Controller {
 	}
 
 	@RequestMapping("/callmicroservice2")
-	String displayDefaultMessageWithParameter(@RequestParam String input) {
+	String displayDefaultMessageWithParameter() {
 		// Hitting the microservice2 from microservice1 to fetch the response.
 		// For simplicity we are directly configuring the url earlier.
 		// For production ready applications it should be populated from the AWS param
 		// store or the properties file.
-		final String microservice2Url = "http://microservice2:8082/microservice2";
+		final String microservice2Url = "http://localhost:8082/testcall";
 		final String response = (String) restTemplate.exchange(microservice2Url, HttpMethod.GET, null, String.class)
 				.getBody();
 
--- demo-microservice2/src/main/java/demo/microservice2/controller/Microservice2Controller.java
+++ demo-microservice2/src/main/java/demo/microservice2/controller/Microservice2Controller.java
@@ -3,10 +3,8 @@ package demo.microservice2.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,18 +23,9 @@ public class Microservice2Controller {
 		return message;
 	}
 
-	@RequestMapping("/callmicroservice2")
-	String displayDefaultMessageWithParameter(@RequestParam String input) {
-		// Hitting the microservice2 from microservice1 to fetch the response.
-		// For simplicity we are directly configuring the url earlier.
-		// For production ready applications it should be populated from the AWS param
-		// store or the properties file.
-		final String microservice2Url = "http://microservice2:10092/microservice2";
-		final String response = (String) restTemplate.exchange(microservice2Url, HttpMethod.GET, null, String.class)
-				.getBody();
-
-		LOGGER.info("The response received from microservice2= " + response);
-		// Returning the response to the user.
-		return response;
+	@RequestMapping("/testcall")
+	String callFromExternalSource() {
+		String message = "Hi external service !! Its a response from from Microservice2 !!";
+		return message;
 	}
 }
